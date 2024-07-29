934ace2ddce0112571d7080f835424212d250f74
jacksonamankwaah <amankwaahjackson@gmail.com>
2024-07-29 03:10:44 +0000

    updated Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python3
-  2 """
-  3 a script that starts a Flask web application
-  4 """
-  5 
-  6 from flask import Flask
-  7 app = Flask(__name__)
-  8 
-  9 
- 10 @app.route('/', strict_slashes=False)
- 11 def index():
- 12     """returns Hello HBNB!"""
- 13     return 'Hello HBNB!'
- 14 
- 15 if __name__ == '__main__':
- 16     app.run(host='0.0.0.0', port='5000')
+!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    Displays 'Hello HBNB!'.
+
+    """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
